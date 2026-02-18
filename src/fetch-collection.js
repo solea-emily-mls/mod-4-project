13a655d1f01e04076026808ce052617440f133b7
Emily Vu <evu725@gmail.com>
2026-02-18 15:49:07 -0500

    Add fetch function for getting a single artwork by id

--- src/fetch-collection.js
+++ src/fetch-collection.js
@@ -20,3 +20,27 @@ export const getData = async () => {
         console.warn('Error fetching artworks: ', error);
     }
 } 
+
+export const getArtById = async (id) => {
+    try {
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
+        if (!response.ok) {
+            throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+        }
+
+        const { data: artwork } = await response.json();
+
+        const artworkInfo = {
+            title: artwork.title,
+            image_id: artwork.image_id,
+            date_display: artwork.date_display,
+            place_of_origin: artwork.place_of_origin,
+            short_description: artwork.short_description
+        };
+
+        return artworkInfo;
+
+    } catch (error) {
+        console.warn('Error fetching artwork: ', error);
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { getData } from './fetch-collection.js';
+import { getArtById } from './fetch-collection.js';
 
 const getArtworks = async () => {
       const artworks = await getData();
       console.log(artworks);
 }
 
-getArtworks()
+const getArt = async (id) => {
+  const artworkInfo = await getArtById(`${id}`);
+  console.log(artworkInfo);
+}
+
+// getArt("129884")
+// getArtworks()
