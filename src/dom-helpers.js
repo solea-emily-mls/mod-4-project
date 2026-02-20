b4bffafbf770534bb7d91ad0d7ebd9daaf334aa7
Solea Miller <soleamiller@gmail.com>
2026-02-20 10:32:00 -0500

    added a filter function + formatting

--- src/dom-helpers.js
+++ src/dom-helpers.js
@@ -1,47 +1,47 @@
-const collectionList = document.querySelector('#collection-list');
-const errorEL = document.querySelector('#error');
-const artwork = document.querySelector('#single-work')
-const card = document.querySelector('#card');
+const collectionList = document.querySelector("#collection-list");
+const errorEL = document.querySelector("#error");
+const artwork = document.querySelector("#single-work");
+const card = document.querySelector("#card");
 
 export const renderCollection = (artworks) => {
-    collectionList.innerHTML= '';
+  collectionList.innerHTML = "";
 
-    artworks.forEach((work) => {
-        const li = document.createElement('li');
+  artworks.forEach((work) => {
+    const li = document.createElement("li");
 
-        li.dataset.workId = work.id;
+    li.dataset.workId = work.id;
 
-        const img = document.createElement('img');
-        img.src = `https://www.artic.edu/iiif/2/${work.image_id}/full/843,/0/default.jpg`;
-        img.alt = work.title;
+    const img = document.createElement("img");
+    img.src = `https://www.artic.edu/iiif/2/${work.image_id}/full/843,/0/default.jpg`;
+    img.alt = work.title;
 
-        const h3 = document.createElement('h3');
-        h3.textContent = work.title;
+    const h3 = document.createElement("h3");
+    h3.textContent = work.title;
 
-        li.append(img, h3);
-        collectionList.append(li);
-    });
+    li.append(img, h3);
+    collectionList.append(li);
+  });
 };
 
 export const renderArtwork = (work) => {
-    artwork.innerHTML = '';
+  artwork.innerHTML = "";
 
-    const img = document.createElement('img');
-    img.src = `https://www.artic.edu/iiif/2/${work.image_id}/full/1686,/0/default.jpg`;
-    img.alt = work.title;
+  const img = document.createElement("img");
+  img.src = `https://www.artic.edu/iiif/2/${work.image_id}/full/1686,/0/default.jpg`;
+  img.alt = work.title;
 
-    const title = document.createElement('h3');
-    title.textContent = work.title;
+  const title = document.createElement("h3");
+  title.textContent = work.title;
 
-    const info = document.createElement('h4')
-    info.textContent = `${work.place_of_origin || 'Unknown origin'},  ${work.date_display || 'Date unknown'}`;
-    const description = document.createElement('p');
-    description.textContent = `${work.short_description || 'No description available'}`;
+  const info = document.createElement("h4");
+  info.textContent = `${work.place_of_origin || "Unknown origin"},  ${work.date_display || "Date unknown"}`;
+  const description = document.createElement("p");
+  description.textContent = `${work.short_description || "No description available"}`;
 
-    card.append(img, title, info, description);
-    artwork.append(card);
+  card.append(img, title, info, description);
+  artwork.append(card);
 };
 
 export const renderError = (msg) => {
-    errorEL.textContent = msg;
-}
+  errorEL.textContent = msg;
+};
--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -1,47 +1,46 @@
 export const getData = async () => {
-    try {
-        const response = await fetch('https://api.artic.edu/api/v1/artworks');
-        if (!response.ok) {
-            throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
-        }
-        const { data: artworksArray } = await response.json();
-        console.log(artworksArray);
-
-        const artworks = artworksArray.map(artwork => ({
-            id: artwork.id,
-            title: artwork.title,
-            artwork_type: artwork.artwork_type_title,
-            image_id: artwork.image_id
-        }));
-
-        return artworks;
+  try {
+    const response = await fetch("https://api.artic.edu/api/v1/artworks");
+    if (!response.ok) {
+      throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     }
-    catch (error) {
-        console.warn('Error fetching artworks: ', error);
-    }
-} 
+    const { data: artworksArray } = await response.json();
+    console.log(artworksArray);
 
-export const getArtById = async (id) => {
-    try {
-        const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
-        if (!response.ok) {
-            throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
-        }
+    const artworks = artworksArray
+      .filter((artwork) => artwork.image_id)
+      .map((artwork) => ({
+        id: artwork.id,
+        title: artwork.title,
+        artwork_type: artwork.artwork_type_title,
+        image_id: artwork.image_id,
+      }));
 
-        const { data: artwork } = await response.json();
-
-        const artworkInfo = {
-            title: artwork.title,
-            image_id: artwork.image_id,
-            date_display: artwork.date_display,
-            place_of_origin: artwork.place_of_origin,
-            short_description: artwork.short_description
-        };
-
-        return artworkInfo;
-
-    } catch (error) {
-        console.warn('Error fetching artwork: ', error);
-    }
+    return artworks;
+  } catch (error) {
+    console.warn("Error fetching artworks: ", error);
+  }
 };
 
+export const getArtById = async (id) => {
+  try {
+    const response = await fetch(`https://api.artic.edu/api/v1/artworks/${id}`);
+    if (!response.ok) {
+      throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+    }
+
+    const { data: artwork } = await response.json();
+
+    const artworkInfo = {
+      title: artwork.title,
+      image_id: artwork.image_id,
+      date_display: artwork.date_display,
+      place_of_origin: artwork.place_of_origin,
+      short_description: artwork.short_description,
+    };
+
+    return artworkInfo;
+  } catch (error) {
+    console.warn("Error fetching artwork: ", error);
+  }
+};
